e19218a7d028eb13139e6a504a83f59e199f0861
Ilya21 <6967221@gmail.com>
2020-10-18 22:14:18 +0200

    init- solution for brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split(''); //
+  for (var i = 0; i < str.length; i++) {
+    for (var [current, next] of bracketsConfig) {
+      if ( next === str[i+1] &&   current === str[i] ) {
+        str.splice(i, 2);
+        i -= 2;
+      }
+    }
+  }
+  return (str.length === 0);
 }
